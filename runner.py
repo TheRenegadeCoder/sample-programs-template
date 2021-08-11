9616702bffa06de799e653fd2f0fca82b3b633e2
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-10 21:43:30 -0400

    Copied a ton of code over

---
+++ runner.py
@@ -0,0 +1,44 @@
+from enum import Enum, auto
+from glotter import main as glotter_main
+
+
+class ProjectType(Enum):
+    Baklava = auto()
+    BinarySearch = auto()
+    BubbleSort = auto()
+    Capitalize = auto()
+    ConvexHull = auto()
+    EvenOdd = auto()
+    Factorial = auto()
+    Fibonacci = auto()
+    FileIO = auto()
+    FizzBuzz = auto()
+    HelloWorld = auto()
+    InsertionSort = auto()
+    JobSequencing = auto()
+    LCS = auto()
+    LPS = auto()
+    LinearSearch = auto()
+    MergeSort = auto()
+    MST = auto()
+    PalindromicNumber = auto()
+    Prime = auto()
+    QuickSort = auto()
+    Quine = auto()
+    ROT13 = auto()
+    ReverseString = auto()
+    RomanNumeral = auto()
+    SelectionSort = auto()
+    SleepSort = auto()
+
+    @property
+    def key(self):
+        return self.name.lower()
+
+
+def main():
+    glotter_main()
+
+
+if __name__ == '__main__':
+    main()
